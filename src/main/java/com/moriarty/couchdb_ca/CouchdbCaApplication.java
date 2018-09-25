9741eda3712ee42b092087b0a0b7576bfc104d3d
darrenmoriarty <dmamprop@gmail.com>
2018-09-25 20:30:47 +0100

    Initial Commit

---
+++ src/main/java/com/moriarty/couchdb_ca/CouchdbCaApplication.java
@@ -0,0 +1,12 @@
+package com.moriarty.couchdb_ca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouchdbCaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouchdbCaApplication.class, args);
+    }
+}
